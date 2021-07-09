61ddee861f64c0d30d1be34a4d7ad742ba929021
JakeSCahill <jakecahillgh@gmail.com>
2021-07-09 15:48:58 +0100

    Bring content up to date and general fixes

--- docs/modules/ROOT/examples/storage/SampleHotRestartConfiguration.java
+++ docs/modules/ROOT/examples/storage/SampleHotRestartConfiguration.java
@@ -2,26 +2,26 @@ import com.hazelcast.config.*;
 
 import java.io.File;
 
-public class SampleHotRestartConfiguration {
+public class SamplePersistenceConfiguration {
 
     public static void main(String[] args) throws Exception{
         //tag::hrconf[]
         Config config = new Config();
-        HotRestartPersistenceConfig hotRestartPersistenceConfig = new HotRestartPersistenceConfig()
+        PersistenceConfig PersistenceConfig = new PersistenceConfig()
         .setEnabled(true)
-        .setBaseDir(new File("/mnt/hot-restart"))
+        .setBaseDir(new File("/mnt/persistence"))
         .setParallelism(1)
         .setValidationTimeoutSeconds(120)
         .setDataLoadTimeoutSeconds(900)
-        .setClusterDataRecoveryPolicy(HotRestartClusterDataRecoveryPolicy.FULL_RECOVERY_ONLY)
+        .setClusterDataRecoveryPolicy(PersistenceClusterDataRecoveryPolicy.FULL_RECOVERY_ONLY)
         .setAutoRemoveStaleData(true);
-        config.setHotRestartPersistenceConfig(hotRestartPersistenceConfig);
+        config.setPersistenceConfig(PersistenceConfig);
 
         MapConfig mapConfig = config.getMapConfig("test-map");
-        mapConfig.getHotRestartConfig().setEnabled(true);
+        mapConfig.getDataPersistenceConfig().setEnabled(true);
 
         CacheSimpleConfig cacheConfig = config.getCacheConfig("test-cache");
-        cacheConfig.getHotRestartConfig().setEnabled(true);
+        cacheConfig.getDataPersistenceConfig().setEnabled(true);
         //end::hrconf[]
     }
 }

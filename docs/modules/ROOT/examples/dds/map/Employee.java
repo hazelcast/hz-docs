026f3343703c7d870576f9b811ada59ee4122470
Serkan Özel <serkan.ozel@hazelcast.com>
2022-11-04 17:36:11 +0300

    Recent changes

--- docs/modules/ROOT/examples/dds/map/Employee.java
+++ docs/modules/ROOT/examples/dds/map/Employee.java
@@ -1,13 +1,6 @@
 //tag::emp[]
 public class Employee {
-
-    private final String surname;
-
-    public Employee(String surname) {
-        this.surname = surname;
-    }
-
-    public static HazelcastJsonValue toHazelcastJsonValue(String surname) {
+    public static HazelcastJsonValue asJson(String surname) {
         return new HazelcastJsonValue("{ \"surname\": \"" + surname + "\" }");
     }
 }

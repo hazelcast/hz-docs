af34ae34b3caef00a5b6acf2a793f650bfed5686
Angela Simms <angela.simms@hazelcast.com>
2023-05-24 17:53:38 +0100

    DOCS-523 Review comments

--- docs/modules/ROOT/examples/storage/SampleHotRestartConfiguration.java
+++ docs/modules/ROOT/examples/storage/SampleHotRestartConfiguration.java
@@ -30,10 +30,7 @@ public class SamplePersistenceConfiguration {
         cacheConfig.getMerkleTreeConfig().setDepth(12);
         config.addCacheConfig(cacheConfig);
 
-        MapConfig mapConfig = config.getMapConfig("__sql.catalog");
-        mapConfig.getDataPersistenceConfig().setEnabled(true);
-        mapConfig.getSqlConfig().setEnabled(true);
-        config.addMapConfig(mapConfig);
+        config.getSqlConfig().setCatalogPersistenceEnabled(true);
 
         config.getJetConfig().setLosslessRestartEnabled(true);
         //end::hrconf[]

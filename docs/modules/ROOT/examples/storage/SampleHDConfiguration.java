3f1ac5273d51dca65bb517c124f9c2ab9a5a50f7
Jack <jack.green@hazelcast.com>
2023-09-20 12:58:29 +0100

    Replace `MemorySize` with `Capacity` (#849)

--- docs/modules/ROOT/examples/storage/SampleHDConfiguration.java
+++ docs/modules/ROOT/examples/storage/SampleHDConfiguration.java
@@ -1,16 +1,16 @@
 import com.hazelcast.config.NativeMemoryConfig;
-import com.hazelcast.memory.MemorySize;
+import com.hazelcast.memory.Capacity;
 import com.hazelcast.memory.MemoryUnit;
 
 public class SampleHDConfiguration {
 
     public static void main(String[] args) throws Exception{
         //tag::hdconf[]
-        MemorySize memorySize = new MemorySize(512, MemoryUnit.MEGABYTES);
+        Capacity capacity = new Capacity(512, MemoryUnit.MEGABYTES);
         NativeMemoryConfig nativeMemoryConfig =
                 new NativeMemoryConfig()
                         .setAllocatorType(NativeMemoryConfig.MemoryAllocatorType.POOLED)
-                        .setSize(memorySize)
+                        .setCapacity(capacity)
                         .setEnabled(true)
                         .setMinBlockSize(16)
                         .setPageSize(1 << 20);

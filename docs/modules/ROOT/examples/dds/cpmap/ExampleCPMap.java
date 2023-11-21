710c2c8c6c3d3ef3067316d9b61ca4070b369f4e
Granville Barnett <granville.barnett@hazelcast.com>
2023-11-21 11:42:49 +0000

    initial sketch of docs

---
+++ docs/modules/ROOT/examples/dds/cpmap/ExampleCPMap.java
@@ -0,0 +1,25 @@
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.cp.CPMap;
+
+public class ExampleCPMap {
+
+    public static void main( String[] args ) {
+        //tag::cpm[]
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        // creates 'capitalCities' CPMap in the 'default' CP Group
+        CPMap<String, String> capitalCities = hazelcastInstance.getCPSubsystem().getMap("capitalCities");
+        // prefer 'set' over 'put' when the previous value associated with a key is not required
+        capitalCities.set("England", "London");
+        assert "London".equals(capitalCities.get("England"));
+        assert null == capitalCities.put("France", "Paris");
+        assert "London".equals(capitalCities.remove("England"));
+        // prefer 'delete' over 'remove' when the value associated with the key is not required
+        capitalCities.delete("France");
+        capitalCities.set("Germany", "Munich");
+        assert capitalCities.compareAndSet("Germany", "Munich", "Berlin");
+        assert "Berlin".equals(capitalCities.get("Germany"));
+        map.destroy();
+        //end::cpm[]
+    }
+}

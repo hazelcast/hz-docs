91d597ae52bc3100d091b13f2ee291d631acac2a
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-12-17 16:59:51 +0100

    Modify version used

--- docs/modules/ROOT/examples/spring/HazelcastHttpSessionConfig.java
+++ docs/modules/ROOT/examples/spring/HazelcastHttpSessionConfig.java
@@ -33,7 +33,6 @@ import com.hazelcast.spring.session.config.annotation.web.http.EnableHazelcastHt
 @Configuration
 public class HazelcastHttpSessionConfig {
 
-
 	@Bean
 	public HazelcastInstance hazelcastInstance() {
 		Config config = new Config();
